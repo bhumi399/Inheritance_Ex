e991057eca137794004fc715b3f82eea50e66e6a
bhumi399 <bhumi399@gmail.com>
2019-07-01 21:42:33 -0500

    Add files via upload

---
+++ Inheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Small_Programs
+{
+    public class Inheritance
+    {
+        public Inheritance()
+        {
+            Console.WriteLine("Calling the human being class constructor");
+        }
+        public void Display()
+        {
+            Console.WriteLine("I'm a human being");
+        }
+    }
+
+    public class Man : Inheritance
+    {
+        public Man()
+        {
+            Console.WriteLine("I'm a man, a male human being");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Man yann = new Man();
+            yann.Display();
+            Console.ReadKey();
+        }
+    }
+}
